305ac0b18b9b301f30221f944aa011721812044f
Artyom <95100288+rambaDev@users.noreply.github.com>
2022-07-06 12:44:33 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-tg_bot_token = "721299835:AAExB9wUY4vCfXbPm_GaNRAvYE8zfamL1cI"
-
-CANNEL_URL = "https://t.me/OmArtVall"
-CHANNEL_ID = "@OmArtVall"
-CHAT_ID = "-1001742324849"
-
-ADMIN_ID = "586214841"
-
-WORLDS = ['хуйло', 'русня']
